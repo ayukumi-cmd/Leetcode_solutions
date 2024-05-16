b3e6f7cd34ad6f413b5eea90b7edda742d28195b
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-05-16 10:56:26 +0530

    Time: 4 ms (82.99%), Space: 11.7 MB (38.15%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> kmp(string s) {
+        vector<int> lps(s.size(), 0);
+        for (int i = 1; i < lps.size(); i++) {
+            int prev_idx = lps[i - 1];
+            while (prev_idx > 0 && s[i] != s[prev_idx]) {
+                prev_idx = lps[prev_idx - 1];
+            }
+            lps[i] = prev_idx + (s[i] == s[prev_idx] ? 1 : 0);
+        }
+        return lps;
+    }
+
+    string shortestPalindrome(string s) {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        vector<int> lps = kmp(s + "#" + s2);
+        string ans;
+        ans+=s2.substr(0, s.size()-lps[lps.size()-1]);
+        ans+=s;
+        return ans;
+    }
+};
