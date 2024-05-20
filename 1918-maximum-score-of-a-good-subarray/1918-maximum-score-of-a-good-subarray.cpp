7d4273e40fcd8e62a5a5e23d8595e9cc7acef845
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-05-21 03:16:21 +0530

    Time: 168 ms (23.93%), Space: 103.1 MB (16.77%) - LeetHub

---
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+  vector<int> PrevSmaller(vector<int> &arr, int n) {
+        stack<int> S;
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++) {
+            while (!S.empty() && arr[S.top()] >= arr[i]){  S.pop();  }  
+            if (S.empty()){ans[i] = -1;}
+            else ans[i] = S.top();
+            S.push(i);
+        }
+        return ans;
+    }
+
+    vector<int> nextsmaller(vector<int> &arr, int n) {
+        stack<int> S;
+        vector<int> ans(n);
+        for (int i = n - 1; i >= 0; i--) {
+            while (!S.empty() && arr[S.top()] >= arr[i]){S.pop();}
+            if (S.empty()){ans[i] = n;}
+            else ans[i] = S.top();
+            S.push(i);
+        }
+        return ans;
+    }
+
+    int largestRectangleArea(vector<int>& heights, int k) {
+        int n = heights.size();
+        vector<int> prevs = PrevSmaller(heights, n);
+        vector<int> nexts = nextsmaller(heights, n);
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+            int width = nexts[i] - prevs[i] - 1;
+            if(prevs[i]<=k-1 and nexts[i]>=k+1){
+            ans = max(ans, height * width);
+            } 
+        }
+        return ans;
+    }
+    int maximumScore(vector<int>& nums, int k) {
+        return largestRectangleArea(nums, k);
+    }
+};
