e2d64a35414e6f3b278314f64efb75abc3673e68
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-03 00:41:20 +0530

    Time: 2043 ms (18.47%), Space: 10.9 MB (98.62%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum = 0;
+        sum = accumulate(nums.begin(), nums.end(), sum);
+        if (nums.size() < k || sum % k) return false;
+        vector<int>visited(nums.size(), false);
+        sort(nums.begin(), nums.end());
+        return backtrack(nums, visited, sum / k, 0, 0, k);
+    }
+    
+    bool backtrack(vector<int>& nums,vector<int>&visited, int target, int curr_sum, int i, int k) {
+        if (k == 1) 
+            return true;
+         if(i>=nums.size()){return false;}
+        if (curr_sum == target) 
+            return backtrack(nums, visited, target, 0, 0, k-1);
+        
+        
+        for (int j = i; j < nums.size(); j++) {
+            if (visited[j] ) continue;
+            if(curr_sum + nums[j] > target){break;}
+           
+            visited[j] = true;
+            if (backtrack(nums, visited, target, curr_sum + nums[j], j+1, k)) return true;
+            visited[j] = false;
+        }
+        
+        return false;
+    }
+};
