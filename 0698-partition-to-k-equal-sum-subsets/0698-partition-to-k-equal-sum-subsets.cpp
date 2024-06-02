3cd502b72f5ee984b0ee556a90feec6c830a64e4
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-03 02:06:21 +0530

    Time: 64 ms (67.21%), Space: 103.7 MB (5.44%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int sum = 0;
-        sum = accumulate(nums.begin(), nums.end(), sum);
-        if (nums.size() < k || sum % k) return false;
-        vector<int>visited(nums.size(), false);
-        sort(nums.begin(), nums.end());
-        return backtrack(nums, visited, sum / k, 0, 0, k);
-    }
-    
-    bool backtrack(vector<int>& nums,vector<int>&visited, int target, int curr_sum, int i, int k) {
-        if (k == 1) 
-            return true;
-        
-        if (curr_sum == target) 
-            return backtrack(nums, visited, target, 0, 0, k-1);
-        
-        
-        for (int j = i; j < nums.size(); j++) {
-            if (visited[j] ) continue;
-            if(curr_sum + nums[j] > target){break;}
-           
-            visited[j] = true;
-            if (backtrack(nums, visited, target, curr_sum + nums[j], j+1, k)) return true;
-            visited[j] = false;
+    int m,d;
+    vector<vector<int>>dp;
+    bool solve(int mask, int score, int k, vector<int>&nums){
+    if(k==1){return true;}
+    if(dp[k][mask]!=-1){return dp[k][mask];}
+    bool ans=false;
+    for(int i=0; i<nums.size(); i++){
+        int cmask= 1<<i;
+        if((cmask&mask)==0){
+            if(score+nums[i]==d){
+                return dp[k][mask]= solve(mask|cmask,0,k-1, nums);
+            }
+            else if(score+nums[i]<d){
+               ans|=solve(mask|cmask, score+nums[i], k, nums);
+            }
         }
-        
-        return false;
     }
-};
+    return dp[k][mask]=ans;
+    }
+   bool canPartitionKSubsets(vector<int>& nums, int k) {
+     int sum=0; 
+     for(int i=0; i<nums.size(); i++){
+        sum+=nums[i];
+     }
+     if(sum%k!=0){return false;}
+     d= sum/k;
+
+    // if((m*(m+1))/2 < d) return false;
+    dp.assign(k+1, vector<int>(1<<(nums.size()), -1));
+     return solve(0, 0, k, nums);
+    }
+}; 
