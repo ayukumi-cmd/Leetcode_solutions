9b63b0d9e77b983f8ae1f6ae957725756981566f
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-08 14:05:30 +0530

    Time: 115 ms (51.16%), Space: 104.1 MB (8.53%) - LeetHub

--- 3248-count-the-number-of-incremovable-subarrays-ii/3248-count-the-number-of-incremovable-subarrays-ii.cpp
+++ 3248-count-the-number-of-incremovable-subarrays-ii/3248-count-the-number-of-incremovable-subarrays-ii.cpp
@@ -1,29 +1,27 @@
 #define ll long long 
 class Solution {
 public:
-pair<ll, ll> find_idx(vector<int>& nums) {
-    long long n = nums.size();
-    long long initial = 0;
-    long long end = n - 1;
-    
-    // Find the end of the longest increasing prefix
-    while (initial < n - 1 && nums[initial] < nums[initial + 1]) {
-        initial++;
-    }
-    
-    // Find the start of the longest decreasing suffix
-    while (end > 0 && nums[end] >nums[end - 1]) {
-        end--;
-    }
-    
-    // If initial and end cross, adjust accordingly
-    if (initial >= end) {
-        initial = n - 1;
-        end = 0;
+    pair<ll,ll>find_idx(vector<int>&nums){
+       ll n= nums.size(); ll initial=0; ll end= n-1;
+        for(int i=1; i<n; i++){
+           if(nums[i]<=nums[i-1]){
+            break;
+           }
+           else{
+            initial++;
+           }
+        }
+        for(int i=n-2; i>=0; i--){
+           if(nums[i]>=nums[i+1]){
+            break;
+           }
+           else{
+            end--;
+           }
+        }
+        return make_pair(initial, end);
     }
 
-    return make_pair(initial, end);
-}
     long long incremovableSubarrayCount(vector<int>& nums) {
         ll n= nums.size();ll ans=0;
         auto p=find_idx(nums); ll init=p.first; ll end=p.second;
