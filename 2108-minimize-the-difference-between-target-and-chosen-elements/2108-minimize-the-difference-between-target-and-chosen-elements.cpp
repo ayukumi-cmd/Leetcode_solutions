3c8e0cc299c3b688391852c3e1e2b29a9d4b09c3
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-09 23:52:42 +0530

    Time: 56 ms (98.03%), Space: 15.9 MB (97.05%) - LeetHub

---
+++ 2108-minimize-the-difference-between-target-and-chosen-elements/2108-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -0,0 +1,26 @@
+
+using namespace std;
+
+class Solution {
+public:
+    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+        bitset<4901> p;
+        p[0] = 1;  // Proper initialization of bitset
+        
+        for (auto& row : mat) {
+            bitset<4901> pp;
+            for (int i : row) {
+                pp |= (p << i);  // Shift p by i and OR it with pp
+            }
+            swap(pp, p);  // Swap pp into p
+        }
+        
+        int res = INT_MAX;
+        for (int i = 0; i < 4901; i++) {
+            if (p[i]) {
+                res = min(res, abs(i - target));
+            }
+        }
+        return res;
+    }
+};
