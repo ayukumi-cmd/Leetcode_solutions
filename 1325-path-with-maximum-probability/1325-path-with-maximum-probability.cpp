06087b8882b8c6383f615144d9ed9ef3e72f6b6c
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-12 12:43:06 +0530

    Time: 151 ms (26.04%), Space: 70 MB (50.28%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -1,47 +1,3 @@
-// class Solution {
-// public:
-//     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-//         vector<pair<int,double>> adj[n+1];
-//         for(int i=0;i<edges.size();i++){
-//             int u=edges[i][0];
-//             int v=edges[i][1];
-//             double t=succProb[i];
-//             adj[u].push_back({v,t});
-//             adj[v].push_back({u,t});
-//         }
-//         vector<double> dist(n+1,0);
-//         dist[start_node]=1;
-//         priority_queue<pair<double,int>>pq;
-//         pq.push({1,start_node});
-//         // stack<int> s;
-//         // vector<int> vis(n+1,0);
-//         // dfs(adj,vis,k,s);
-//         while(!pq.empty()){
-//             auto it=pq.top();
-//             int tt=it.first;
-//             int ngh=it.second;
-//             pq.pop();
-//             for(auto x:adj[ngh]){
-//                 int v=x.first;
-//                 int t=x.second;
-//                 if(t*tt>dist[v]){
-//                     dist[v]=tt*t;
-//                     pq.push({tt*t,v});
-//                 }
-                
-//             }
-//         }
-//         if(dist[end_node]==-1*1e9){return 0;}
-//         return dist[end_node];     
-//     }
-// };
-
-
-#include <vector>
-#include <queue>
-#include <utility>
-using namespace std;
-
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
@@ -67,7 +23,7 @@ public:
             auto [currentProb, node] = pq.top();
             pq.pop();
 
-          
+           // Early exit if end_node is reached
 
             for (auto& [neighbor, prob] : adj[node]) {
                 double newProb = currentProb * prob;
