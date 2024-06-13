7daccde4b00b61640caf1c1fb8a123922e22defe
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-13 14:00:12 +0530

    Time: 9 ms (21.73%), Space: 15.1 MB (27.94%) - LeetHub

--- 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
@@ -1,57 +1,49 @@
 class Solution {
 public:
-    void unionset(int u, int v, vector<int>& parent, vector<int>& rank) {
-        u = findpar(parent, u);
-        v = findpar(parent, v);
-        if (u == v) {
-            return;
-        }
-        if (rank[u] < rank[v]) {
-            parent[u] = v;
-        } else if (rank[v] < rank[u]) {
-            parent[v] = u;
-        } else {
-            parent[v] = u;
-            rank[u]++;
-        }
+    void unionset(int u, int v, vector<int>&parent, vector<int>&rank){
+        u=findpar(parent, u);
+        v=findpar(parent, v);
+        if(u==v){return ;}
+        if(rank[u]<rank[v]){parent[u]=v;}
+        else if(rank[v]<rank[u]){ parent[v]=u;}
+        else {parent[v]=u; rank[u]++;}
+        return ;
     }
 
-    int findpar(vector<int>& parent, int node) {
-        if (parent[node] == node) {
+    int findpar(vector<int>&parent, int node){
+        if(parent[node]==node){
             return node;
         }
-        parent[node] = findpar(parent, parent[node]); // Path compression
+        parent[node]=findpar(parent, parent[node]);
         return parent[node];
     }
 
     bool equationsPossible(vector<string>& equations) {
-        vector<int> parent(26);
-        vector<int> rank(26, 0);
-
-        for (int i = 0; i < 26; i++) {
-            parent[i] = i;
-        }
-
-        // Union for equations with '=='
-        for (const string& eq : equations) {
-            if (eq[1] == '=') {
-                int u = eq[0] - 'a';
-                int v = eq[3] - 'a';
-                unionset(u, v, parent, rank);
+        int n=equations.size();
+        vector<int>parent(26, 0);
+        for(int i=0; i<26; i++){parent[i]=i;}
+        vector<int>rank(26, 0);
+        for(int i=0; i<n; i++){
+            int u=(equations[i][0]-'a');
+            int v=(equations[i][3]-'a');
+            if(equations[i][1]=='='){
+              int paru=findpar(parent, u);
+              int parv=findpar(parent, v);
+              if(paru!=parv){
+                unionset(u, v, parent , rank);
+              }
             }
         }
 
-        // Check for contradictions in equations with '!='
-        for (const string& eq : equations) {
-            if (eq[1] == '!') {
-                int u = eq[0] - 'a';
-                int v = eq[3] - 'a';
-                if (findpar(parent, u) == findpar(parent, v)) {
-                    return false;
-                }
+        for(int i=0; i<n; i++){
+            int u=(equations[i][0]-'a');
+            int v=(equations[i][3]-'a');
+            if(equations[i][1]=='!'){
+              int paru=findpar(parent, u);
+              int parv=findpar(parent, v);
+              if(paru==parv){return false;}
             }
         }
-
         return true;
     }
 };
