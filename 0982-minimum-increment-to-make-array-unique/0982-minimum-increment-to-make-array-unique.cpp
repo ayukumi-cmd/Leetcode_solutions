0ab7ea7451f15a8c2c78162236cccb64092f5c78
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-14 11:22:53 +0530

    Time: 84 ms (96.85%), Space: 74 MB (13.98%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int ans = 0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]<=nums[i-1]){
-                ans += nums[i-1]-nums[i]+1;
-                nums[i]= nums[i-1]+1;
-            }
-        }
-        return ans;
-        }
+        int n= nums.size();
+        int maxi=*max_element(nums.begin(), nums.end());
+        vector<int>freq(maxi+n+1);
+
+        for(int i=0; i<nums.size(); i++){freq[nums[i]]++;}
+
+       int extra=0; int ans=0;
+       for(int i=0; i<freq.size(); i++){
+        if(freq[i]<=1){continue;}
+
+        extra= freq[i]-1;
+        ans+=extra;
+        freq[i+1]+=extra;
+
+       }
+       return  ans;
+    }
 };
