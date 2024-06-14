f6b875b5ab77ec77d70fdf0a4a70888d1651b3f2
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-14 16:51:46 +0530

    Time: 6 ms (36.71%), Space: 13.8 MB (11.79%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -4,10 +4,13 @@ int minPatches(vector<int>& nums, int n) {
     int cnt=0,i=0;
     long long maxNum=0;
     while (maxNum<n){
-       if (i<nums.size() && nums[i]<=maxNum+1)
-            maxNum+=nums[i++];
+       if (i<nums.size() && nums[i]<=maxNum+1){
+            maxNum+=nums[i];
+            i++;
+       }
        else{
-            maxNum+=maxNum+1;cnt++;
+            maxNum+=maxNum+1;
+            cnt++;
        }
    }
    return cnt;
