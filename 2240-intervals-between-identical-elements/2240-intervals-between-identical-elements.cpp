03629302ad1d79a5bf3c340ec2cc6293272b46d7
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-19 15:35:27 +0530

    Time: 254 ms (52.77%), Space: 128 MB (78.27%) - LeetHub

---
+++ 2240-intervals-between-identical-elements/2240-intervals-between-identical-elements.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+     vector<long long> getDistances(vector<int>& nums) {
+        long long   n = nums.size();
+        vector<long long> ans(n, 0);
+        unordered_map<long long , vector<long long >> mp;
+        
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+
+        for (auto& it : mp) {
+            vector<long long >& indexes = it.second;
+            long long left = 0;
+            long long right = accumulate(indexes.begin(), indexes.end(), 0LL)-indexes[0];
+            
+            for (long long  i = 0; i < indexes.size(); i++) {
+                long long  left_len = i;
+                long long right_len = indexes.size() - 1 - i;
+                
+                ans[indexes[i]] = (left_len * indexes[i] - left) + (right - right_len * indexes[i]);
+
+               
+                if (i != indexes.size() - 1) {
+                     left += indexes[i];
+                    right -= indexes[i + 1];
+                }
+            }
+        }
+
+        return ans;
+    }
+};
