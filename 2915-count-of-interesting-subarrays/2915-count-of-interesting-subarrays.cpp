751f35ee3a08578efbadd94584caf1c96f0c3b26
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-23 14:41:21 +0530

    Time: 141 ms (92.81%), Space: 117 MB (72.3%) - LeetHub

---
+++ 2915-count-of-interesting-subarrays/2915-count-of-interesting-subarrays.cpp
@@ -0,0 +1,40 @@
+
+class Solution {
+    long long modii = 300000000;
+
+    long long subarraysDivByK(vector<int>& nums, int m, int k) {
+        long long n = nums.size();
+        unordered_map<long long, long long> mp;
+        long long sum = 0;
+        long long res = 0;
+        mp[0] = 1; // To count subarrays that directly start from index 0
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            long long modValue = (sum % m + m) % m;
+            long long target = (modValue - k + m) % m;
+
+            if (mp.find(target) != mp.end()) {
+                res += mp[target];
+            }
+
+            mp[modValue]++;
+        }
+
+        return res;
+    }
+
+public:
+    long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+        long long n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % modulo == k) {
+                nums[i] = 1;
+            } else {
+                nums[i] = 0;
+            }
+        }
+
+        return subarraysDivByK(nums, modulo, k);
+    }
+};
