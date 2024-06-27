68f2657c207e924df4776934cd40b995ab979ce5
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-27 16:21:19 +0530

    Time: 13 ms (68.37%), Space: 20.6 MB (10.29%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,26 +3,28 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> score(n, 1);
+        vector<int> score1(n, 1);
 
         // Left to right pass
         for(int i = 1; i < n; ++i) {
             if(ratings[i] > ratings[i - 1]) {
-                score[i] = score[i - 1] + 1;
+                score1[i] = score1[i - 1] + 1;
             }
         }
 
         // Right to left pass
+        vector<int> score2(n, 1);
         for(int i = n - 2; i >= 0; --i) {
             if(ratings[i] > ratings[i + 1]) {
-                score[i] = max(score[i], score[i + 1] + 1);
+                score2[i] = score2[i + 1] + 1;
             }
         }
 
         // Calculate the total candies needed
         int sum = 0;
         for(int i = 0; i < n; ++i) {
-            sum += score[i];
+            
+            sum += max(score1[i], score2[i]);
         }
 
         return sum;
