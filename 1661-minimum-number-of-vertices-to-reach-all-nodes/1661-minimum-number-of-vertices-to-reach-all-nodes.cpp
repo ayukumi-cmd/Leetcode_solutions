e17bd846d0573bebc6d54e6060b4bcc9c0178d83
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-06-28 13:34:14 +0530

    Time: 230 ms (42.58%), Space: 111.2 MB (23.94%) - LeetHub

---
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,17 @@
+#define ll long long 
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<ll>indegree(n, 0);
+        for(auto it:edges){
+            indegree[it[1]]++;
+        }
+
+        vector<int>ans; 
+        for(int i=0; i<indegree.size(); i++){
+            if(indegree[i]==0){ans.push_back(i);}
+        }
+        return ans;
+
+    }
+};
