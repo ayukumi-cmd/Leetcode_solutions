c691b478d04690099be015a07ac41250bbfb5268
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-07-11 17:11:05 +0530

    Time: 0 ms (100%), Space: 12.1 MB (30.13%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,69 +1,51 @@
 class Solution {
 public:
-    vector<int>ans;
-    
-    bool dfs(int node, int parent, vector<int> adj[], vector<int>& vis, stack<int>st) {
-        vis[node] = 1;
-        st.push(node);
-        
-        for (auto adjn : adj[node]) {
-            if (!vis[adjn]) {
-                if (dfs(adjn, node, adj, vis, st)) {
-                    return true;
-                }
-            } else if (adjn != parent) {
-                
-               while(st.top()!=adjn){
-                ans.push_back(st.top());
-                st.pop();
-               }
-               ans.push_back(st.top());
-               st.pop();
-                
-                return true;
-            }
-        }
-        
-        st.pop();
-        return false;
+    void unionset(int u, int v, vector<int>&parent, vector<int>&rank){
+        u=findpar(parent, u);
+        v=findpar(parent, v);
+        if(u==v){return ;}
+        if(rank[u]<rank[v]){parent[u]=v;}
+        else if(rank[v]<rank[u]){ parent[v]=u;}
+        else {parent[v]=u; rank[u]++;}
+        return ;
     }
 
-    bool iscyle(int v, vector<int> adj[]) {
-        vector<int> vis(v + 1, 0);
-        stack<int>st;
-        
-        for (int i = 1; i <= v; i++) {
-            if (!vis[i]) {
-                if (dfs(i, -1, adj, vis, st)) {
-                    return true;
-                }
-            }
+    int findpar(vector<int>&parent, int node){
+        if(parent[node]==node){
+            return node;
         }
-        return false;
+        parent[node]=findpar(parent, parent[node]);
+        return parent[node];
     }
+    void makeset(vector<int>&parent , vector<int>&rank, int n){
 
+     for(int i=1;i<= n; i++){
+        parent[i]=i;
+        rank[i]=0;
+     }
+
+
+    }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        vector<int> adj[edges.size() +1];
         
-        for (auto it : edges) {
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+      vector<int>parent(edges.size()+1);
+      vector<int>rank(edges.size()+1);
+      makeset(parent, rank, edges.size());
+  
+       vector<int>ans;
+      for(auto it:edges){
+        int u= findpar(parent, it[0]);
+        int v= findpar(parent, it[1]);
+
+        if(u!=v){
+            unionset(u, v, parent, rank);
         }
-
-
-
-        bool x = iscyle(edges.size(), adj);
-
-        unordered_set<int> st(ans.begin(), ans.end());
-        vector<int> res(2, 0);
-        
-        for (auto it : edges) {
-            if (st.find(it[0]) != st.end() && st.find(it[1]) != st.end()) {
-                res[0] = it[0];
-                res[1] = it[1];
-            }
+        else if(u==v){
+         ans.push_back(it[0]);
+         ans.push_back(it[1]);
         }
-        
-        return res;
+      }
+      return ans;
+
     }
 };
