84248876d7e4248b3e0e671be9c95caa812f2d9e
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-07-27 00:33:50 +0530

    Time: 11 ms (78.84%), Space: 29.4 MB (48.48%) - LeetHub

---
+++ 3034-points-that-intersect-with-cars/3034-points-that-intersect-with-cars.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   int numberOfPoints(vector<vector<int>>& nums) {
+    vector<int>line(102, 0);
+    for(auto& p : nums){
+        line[p[0]]++;
+        line[p[1]+1]--;
+    }
+    int ans =0;
+    int count =0;
+    for(int i =0; i <102; ++i){
+        count += line[i];
+        if(count > 0){
+            ++ans;
+        }
+    }
+    return ans;
+}
+
+};
