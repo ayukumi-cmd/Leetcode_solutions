19c85cebcb0011b12f79815b17988e19c3e3c91d
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-07-27 12:45:56 +0530

    Time: 93 ms (24.57%), Space: 20.6 MB (14.29%) - LeetHub

--- 1647-can-convert-string-in-k-moves/1647-can-convert-string-in-k-moves.cpp
+++ 1647-can-convert-string-in-k-moves/1647-can-convert-string-in-k-moves.cpp
@@ -1,29 +1,26 @@
-
-
 class Solution {
 public:
     bool canConvertString(string s, string t, int k) {
-        int n = s.size();
-        if (n != t.size()) {
-            return false; 
-        }
-
-        unordered_map<int, int> mp; 
-
-        for (int i = 0; i < n; i++) {
-            if (s[i] != t[i]) {
-                int shift = (t[i] - s[i] + 26) % 26;
-
-                int totalShift = shift + 26 * mp[shift]; 
-
-                if (totalShift > k) {
-                    return false;
-                }
-
-                mp[shift]++; 
+        int n=s.size();
+        if(s.size()!=t.size()){return false;}
+        int ans=0;
+        unordered_map<int, int>mp;
+        for(int i=0; i<n; i++){
+            if((s[i]-'a')>(t[i]-'a')){
+                int x=(26-(s[i]-'a')+(t[i]-'a'));
+                int y= (x+26*mp[x]);
+                if(y>k){return false;}
+                mp[x]++;
+              
+            }
+            else if((s[i]-'a')<(t[i]-'a')){
+                int x=(t[i]-'a')-(s[i]-'a');
+                int y= (x+26*mp[x]);
+                if(y>k){return false;}
+                mp[x]++;
             }
         }
 
-        return true; 
+        return true;
     }
 };
