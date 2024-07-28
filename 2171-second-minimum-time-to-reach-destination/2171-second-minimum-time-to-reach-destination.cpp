e95749550d3d4b75f55e321e20ca2a8f3bf891a4
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-07-28 10:55:31 +0530

    Time: 456 ms (72.71%), Space: 180.5 MB (94.29%) - LeetHub

--- 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.cpp
+++ 2171-second-minimum-time-to-reach-destination/2171-second-minimum-time-to-reach-destination.cpp
@@ -2,11 +2,11 @@
 class Solution {
 public:
    
-
-    vector<int> dijkstra(const vector<vector<int>>& graph, int source, int time, int change) {
-        int n = graph.size();
-        vector<int> dis(n, INT_MAX);
-        vector<int> dis2(n, INT_MAX);
+   
+    vector<int> dijkstra(vector<int>graph[], int source, int time, int change, int n) {
+        
+        vector<int> dis(n+1, INT_MAX);
+        vector<int> dis2(n+1, INT_MAX);
         priority_queue<PII, vector<PII>, greater<PII>> pq;
         dis[source] = 0;
         pq.push({0, source});
@@ -16,9 +16,11 @@ public:
 
             for (auto nei : graph[node]) {
                 int new_cost = cost + time;
+             //   cout<<cost<<" ";
                 if ((cost / change) % 2 == 1) {
                     new_cost += change - (cost % change);
                 }
+              //  cout<<new_cost<<endl;
 
                 if (dis[nei] > new_cost) {
                     dis2[nei] = dis[nei];
@@ -31,17 +33,18 @@ public:
             }
         }
 
-        return {dis[n-1], dis2[n-1]};// dis[n-1] first min dist and dis2[n-1] is the second min distance
+        return {dis[n], dis2[n]};
     }
 
     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
-        vector<vector<int>> graph(n + 1);
+       vector<int>graph[n+1];
+      
         for (auto &e : edges) {
             graph[e[0]].push_back(e[1]);
             graph[e[1]].push_back(e[0]);
         }
 
-        vector<int> result = dijkstra(graph, 1, time, change);
+        vector<int> result = dijkstra( graph ,1, time, change, n);
         return result[1];
     }
 };
