8f99bb3fde9d8e47d33a23d0ca91fe44566cb919
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-08-01 19:59:06 +0530

    Time: 173 ms (40.54%), Space: 16.8 MB (38.33%) - LeetHub

---
+++ 2648-number-of-ways-to-earn-points/2648-number-of-ways-to-earn-points.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int mod=1e9+7; 
+    
+    int cal(int i ,int n , vector<vector<int>>& types , int sum , int tar  , vector<vector<int>>& dp)
+    {
+        if(i==n)
+        {
+            return tar==sum; 
+        }
+
+        
+      if(sum==tar)
+          return 1; 
+        
+
+        
+        if(dp[i][sum]!=-1)
+            return dp[i][sum] ;
+        
+         int take=0 , nottake=0 ; 
+
+        for(int k=1;k<=types[i][0];k++)
+        {
+            if(sum+k*types[i][1]<=tar){ take=(take+cal(i+1 , n, types , sum+k*types[i][1] , tar, dp))%mod;}
+        }
+        nottake=(nottake+ cal(i+1 , n , types , sum , tar  ,dp ))%mod; 
+        return dp[i][sum] =  (take+nottake)%mod; 
+    }
+    int waysToReachTarget(int target, vector<vector<int>>& types) {
+      vector<vector<int>>dp(types.size() , vector<int>(target+100 , -1)) ; 
+        return cal(0 , types.size(),  types , 0 ,  target , dp  ) ;
+    }
+};
