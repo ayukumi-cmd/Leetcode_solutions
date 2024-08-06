cfb38060c328cf0387714167222886e6c447d281
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-08-06 11:41:28 +0530

    Time: 68 ms (79.47%), Space: 24.2 MB (87.13%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.size();
+        vector<int> v(26, 0);
+        for (int i = 0; i < n; i++) {
+            v[word[i] - 'a']++;
+        }
+        sort(v.begin(), v.end(), greater<int>());
+
+        int cnt = 1;
+        int ans = 0;
+        for (auto i : v) {
+            if (cnt <= 8) {
+                ans += i;
+            } else if (cnt > 8 && cnt <= 16) {
+                ans += i * 2;
+            } else if (cnt > 16 && cnt <= 24) {
+                ans += i * 3;
+            } else {
+                ans += i * 4;
+            }
+            if (i != 0) {
+                cnt++;
+            }
+        }
+        return ans;
+    }
+};
+
