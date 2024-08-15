f31c90168fbb4876e3ba25f0ef5825f81bff6d67
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-08-15 21:16:20 +0530

    Time: 314 ms (66.82%), Space: 0B (100%) - LeetHub

---
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT DISTINCT emp.employee_id
+FROM   employees emp
+       LEFT JOIN employees mng
+              ON( emp.manager_id = mng.employee_id )
+WHERE  emp.manager_id IS NOT NULL
+       AND emp.salary < 30000
+       AND mng.employee_id IS NULL
+ORDER  BY emp.employee_id 
