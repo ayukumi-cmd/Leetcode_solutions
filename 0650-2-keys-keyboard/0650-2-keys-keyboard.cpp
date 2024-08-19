f841bbaf61211e8730e361e3f39613c8b365a0bb
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-08-19 13:22:13 +0530

    Time: 0 ms (100%), Space: 7.5 MB (71.95%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,35 +1,13 @@
 class Solution {
 public:
-    int m;
-    vector<vector<int>>dp;
-    int solve(int x, int y) {
-        if (x > m) {
-            return INT_MAX;
+ int minSteps(int n) {
+        int s = 0;
+        for (int d = 2; d <= n; d++) {
+            while (n % d == 0) {
+                s += d;
+                n /= d;
+            }
         }
-        if (x == m) {
-            return 0;
-        }
-        if(dp[x][y]!=-1){return dp[x][y];}
-        int res = INT_MAX;
-        if (y>0) {
-          
-            int a = solve(x + y, y);
-            if (a != INT_MAX) res = min(res, 1 + a);
-            
-            
-            int b = solve(x + y, x + y);
-            if (b != INT_MAX) res = min(res, 2 + b);
-        } else {
-
-            int c = solve(x, x);
-            if (c != INT_MAX) res = min(res, 1 + c);
-        }
-        return dp[x][y]=res;
-    }
-    
-    int minSteps(int n) {
-        m = n;
-        dp.resize(2000, vector<int>(2000, -1));
-        return solve(1, 0);
+        return s;
     }
 };
