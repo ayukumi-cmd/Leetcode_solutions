c79de0a4024b548565a1ee82623d7df9e23e19f1
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-08-20 02:50:09 +0530

    Time: 16 ms (86.05%), Space: 27.7 MB (46.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,12 +1,21 @@
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> temp(nums.size());
 
-        for(int i=0; i< nums.size() ; i++){
-            temp[(i+k) % nums.size()] = nums[i];
-        }
+        
+        k=k%nums.size();
+        reverse(nums.begin(),nums.begin()+(nums.size()-k));
 
-        nums = temp;
+        reverse(nums.begin()+(nums.size()-k),nums.end());
+    
+        reverse(nums.begin(),nums.end());
+            
+
+        
+        
+
+        
+        
     }
 };
