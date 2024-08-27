ea44a63e80f8687bd52de803725da690642ea241
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-08-28 01:35:06 +0530

    Time: 284 ms (44.24%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,2 +1,4 @@
-# Write your MySQL query statement below
-select name,population,area from World where area>=3000000 or population>24999999;
+-- select name, population, area from World where area>=3000000 and population>=25000000
+SELECT name, population, area 
+FROM World 
+WHERE area >=3000000 or population>=25000000;
