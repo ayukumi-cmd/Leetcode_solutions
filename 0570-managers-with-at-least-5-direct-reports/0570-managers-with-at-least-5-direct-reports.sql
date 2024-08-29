d97f6d76ee130fc1b73038a965e1c2c21c0f3bca
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-08-29 16:37:21 +0530

    Time: 343 ms (54.66%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-# Write your MySQL query statement below
-Select m.name
-from employee as e
-inner join employee as m
-on e.managerId=m.id
-group by e.managerId 
-having count(e.id)>=5
+select e2.name 
+from Employee e1 join Employee e2 
+on e1.managerId= e2.id
+group by e2.id
+having count(e1.id)>=5
