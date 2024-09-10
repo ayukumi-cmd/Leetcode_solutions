f0937a343fa8911750c1af3919fac0d3a743d151
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-09-10 20:46:25 +0530

    Time: 17 ms (94.16%), Space: 30.8 MB (69.16%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
@@ -1,34 +1,24 @@
-
-
 class Solution {
 public:
-    ListNode* head;
-    map<pair<TreeNode*, ListNode*>, bool>mp;
-    bool solve(TreeNode* root, ListNode* temp) {
-        if ( temp == NULL) {
-            return true;
-        }
-        if (root==NULL) {
-          return false;
-        }
-        auto it= make_pair(root, temp);
-        if(mp.count(it)){return mp[it]; }
-        
-        int x= false;
-        if(root->val==temp->val){x=x|| solve(root->right, temp->next)||solve(root->left, temp->next);}
-         if(head->val ==root->val){
-            x=x|| solve(root->right, head->next)||solve(root->left, head->next);
-        }
-        
-             x=x|| solve(root->right, head)||solve(root->left, head);
-    
 
-        return mp[it]=x;
+    bool subPath(ListNode* head,ListNode* curr,TreeNode* root) {
+        if(curr == NULL){return 1;}
+        if(root == NULL){return 0;}
+        
+        if(curr->val == root->val){
+            curr = curr->next;
+        }
+        else if(head->val == root->val){
+            head = head->next;
+        }
+        else{
+            curr = head;
+        } 
+
+        return subPath(head,curr,root->left) || subPath(head,curr,root->right);
     }
-    
-    bool isSubPath(ListNode* h, TreeNode* root) {
-        head = h; 
-        mp.clear();
-        return solve(root, h)||solve(root->left, h)||solve(root->right, h);
+
+    bool isSubPath(ListNode* head,TreeNode* root) {
+        return subPath(head,head,root);
     }
 };
