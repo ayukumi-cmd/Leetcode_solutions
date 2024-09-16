181fc34755d9fc7f8ab16ab260232fc3f1731868
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-09-16 11:24:33 +0530

    Time: 23 ms (6.62%), Space: 18.4 MB (27.31%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMinDifference(vector<string>& times) {
+        int n = times.size();
+        sort(times.begin(), times.end());
+        int mindiff = INT_MAX;
+        for (int i = 0; i < times.size()-1; i++) {
+            int diff = abs(timeDiff(times[(i +1)], times[i]));
+            diff = min(diff, 1440 - diff);
+            mindiff = min(mindiff, diff);
+        }
+        int dif= abs(timeDiff(times[0], times[n-1]));
+        dif = min(dif, 1440 - dif);
+        mindiff= min(mindiff, dif);
+
+        return mindiff;
+    }
+
+private:
+    int timeDiff(string t1, string t2) {
+        int h1 = stoi(t1.substr(0, 2));
+        int m1 = stoi(t1.substr(3, 2));
+        int h2 = stoi(t2.substr(0, 2));
+        int m2 = stoi(t2.substr(3, 2));
+        return (h2 - h1) * 60 + (m2 - m1);
+    }
+};
