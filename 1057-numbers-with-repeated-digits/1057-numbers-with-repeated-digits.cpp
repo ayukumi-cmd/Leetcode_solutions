e9e446d8175aa38f170bfa99262d6a618ec99997
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-09-18 19:56:34 +0530

    Time: 5 ms (65.54%), Space: 7.8 MB (56.76%) - LeetHub

---
+++ 1057-numbers-with-repeated-digits/1057-numbers-with-repeated-digits.cpp
@@ -0,0 +1,43 @@
+
+class Solution {
+public:
+    string r; 
+    int dp[1024][2][2][11]; 
+
+    int solve(int mask, int tight, int leading, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if (dp[mask][tight][leading][n] != -1) {
+            return dp[mask][tight][leading][n];
+        }
+
+        int x = (r[r.length() - n] - '0');
+        int ub = tight ? x : 9;
+        int ans = 0;
+
+        if (leading) {
+            ans += solve(mask, 0, 1, n - 1); 
+            for (int i = 1; i <= ub; i++) {
+                if (!(mask & (1 << i))) {
+                    ans += solve(mask | (1 << i), tight & (i == ub), 0, n - 1);
+                }
+            }
+        } else {
+            for (int i = 0; i <= ub; i++) {
+                if (!(mask & (1 << i))) {
+                    ans += solve(mask | (1 << i), tight & (i == ub), 0, n - 1);
+                }
+            }
+        }
+
+        return dp[mask][tight][leading][n] = ans;
+    }
+
+ int numDupDigitsAtMostN(int n) {
+        r = to_string(n);
+        memset(dp, -1, sizeof(dp));
+       return  n-(solve(0, 1, 1, r.length()) - 1);
+    }
+};
