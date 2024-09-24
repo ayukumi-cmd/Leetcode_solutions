bd96592fd4ff74f978b8af7cf44fd3d8771d68e8
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-09-24 14:27:31 +0530

    Time: 398 ms (46.03%), Space: 229.6 MB (11.18%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,31 +1,43 @@
+class Trienode {
+public:
+    unordered_map<char, Trienode*> child;
+    Trienode(){}
+};
+
 class Solution {
 public:
-    set<string>make_hash(vector<int>&a){
-        set<string>st;
-        for(int i=0; i<a.size(); i++){
-            string temp;
-            string s=to_string(a[i]);
-            for(int j=0; j<s.length(); j++){
-             temp+=s[j];
-             st.insert(temp);
-            }
-
-        }
-        return st;
-    }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        int n=arr1.size();
-        int m=arr2.size();
-        int ans= -1;
-        set<string>st=make_hash(arr1);
-        for(int i=0;i<m; i++){
-            string s=to_string(arr2[i]);
-            string temp;
-            for(int j=0; j<s.length(); j++){
-             temp+=s[j];
-             if(st.find(temp)!=st.end()){ans= max(ans, j+1);}
+        Trienode* root = new Trienode();
+
+        for (int x : arr1) {
+            string word = to_string(x);
+            Trienode* curr = root;
+            for (char c : word) {
+                if (curr->child.find(c) == curr->child.end()) {
+                    curr->child[c] = new Trienode();
+                }
+                curr = curr->child[c];
+            }
+           
+        }
+
+        int ans = 0;
+        for (int x : arr2) {
+            string word = to_string(x);
+            Trienode* curr = root;
+            int cnt = 0;
+            for (char c : word) {
+                if (curr->child.find(c) == curr->child.end()) {
+                    break;
+                }
+                curr = curr->child[c];
+                cnt++;
+           
+                    ans = max(ans, cnt);
+              
             }
         }
-        return ans==-1?0:ans;
+
+        return ans;
     }
 };
