8848bb27b1603e08f8bc66fbf3b5b1d62e05776c
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-09-30 12:43:14 +0530

    Time: 2 ms (60.62%), Space: 7.2 MB (88.92%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
-    int dp[11][2][1 << 10];
-    int solve(int pos, bool bound,  int mask, string s) {
-        if (pos == s.length()) return 1;
-        if (dp[pos][bound][mask] != -1) return dp[pos][bound][mask];
-
-        int ans = 0;
-        int maxDigit = bound ? s[pos] - '0' : 9;
-        for (int digit = 0; digit <= maxDigit; digit++) {
-            if (mask == 0 && digit == 0) ans += solve(pos + 1, bound && (digit == s[pos] - '0'), mask, s);
-            else if (!(mask & (1 << digit))) ans += solve(pos + 1, bound && (digit == s[pos] - '0'), mask | (1 << digit), s);
-        }
-
-        return dp[pos][bound][mask] = ans;
-
-    }
     int countNumbersWithUniqueDigits(int n) {
-        memset(dp, -1, sizeof(dp));
-        if(n==0){return 1;}
-     
-        int num = 1;
-        for (int i = 0; i < n; i++) num *= 10;
-
-        num--;
-        string s = to_string(num);
-        return solve(0, true, 0, s);
+        vector<int>nums(10); 
+        nums[0]=1; 
+        nums[1]= nums[0]+9;
+        nums[2]= nums[1]+9*9;
+        nums[3]= nums[2]+9*9*8;
+        nums[4]= nums[3]+9*9*8*7;
+        nums[5]= nums[4]+9*9*8*7*6;
+        nums[6]= nums[5]+9*9*8*7*6*5;
+        nums[7]= nums[6]+9*9*8*7*6*5*4;
+        nums[8]= nums[7]+9*9*8*7*6*5*4*3;
+        nums[9]= nums[8]+9*9*8*7*6*5*4*3*2;
+        return nums[n];
     }
 };
