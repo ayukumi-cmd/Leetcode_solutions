3de2eb22239564db3217f0bd47aa977b24ddbf6e
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-10-08 15:30:31 +0530

    Time: 106 ms (53.6%), Space: 32.7 MB (54.72%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -3,18 +3,21 @@ public:
     int minSwaps(string s) {
         int n=s.length();
 
-        int open=0; int ans=0;
+        int open=0; 
+        int close=0;
+        int ans=0;
         for(int i=0; i<n; i++){
             if(s[i]=='['){
                 open++;
             }
             else if(s[i]==']') {
-                open--;
+                close++;
             }
 
-            if(open<0){
+            if(open<close){
                 ans++;
-                open=1;
+                open++; 
+                close--;
             }
         }
 
