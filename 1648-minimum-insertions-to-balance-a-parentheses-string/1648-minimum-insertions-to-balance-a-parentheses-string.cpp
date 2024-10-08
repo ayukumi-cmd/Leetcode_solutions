c1fc868fe839f4dffbde0d36766c4720f0061484
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-10-08 18:24:44 +0530

    Time: 334 ms (5.07%), Space: 14.5 MB (51.45%) - LeetHub

--- 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     int minInsertions(string s) {
-        int ans = 0;
-        int n = s.size();
-        stack<char> st;
+        int ans=0; int n= s.size();
+        stack<char>st;
 
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '(') {
-                st.push(s[i]);
-            } else {
-                if (i < n - 1 && s[i + 1] == ')') {
-                    if (st.empty()) {
-                        ans++;
-                    } else {
-                        st.pop();
-                    }
-                    i++;
-                } else {
-                    if (st.empty()) {
-                        ans += 2;
-                    } else {
-                        st.pop();
-                        ans++;
+        for(int i=0; i<n; i++){
+            if(s[i]=='('){st.push(s[i]);}
+
+            else{
+                if(i<n-1 and s[i+1]==')'){
+                    if(st.empty()){ans++;i++;}
+                    else {
+                        i++; st.pop();
                     }
                 }
+                else if(st.empty()){ans+=2;}
+                else if(!st.empty()) {ans++; st.pop();}
             }
+
+            cout<<ans<<endl;
         }
-        return ans + 2 * st.size();
+        return ans+2*st.size();
     }
 };
+
+
