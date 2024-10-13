cb8b3c8ffe05f09fb6dbe296e1227bd0b66140bc
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-10-13 13:22:35 +0530

    Time: 84 ms (14.05%), Space: 27.3 MB (10.19%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+   
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
+        int curMax = INT_MIN; 
+
+        for (int i = 0; i < nums.size(); i++) {
+            minHeap.push({nums[i][0], i, 0});
+            curMax = max(curMax, nums[i][0]);
+        }
+
+        vector<int> smallRange = {-100000, 100000}; 
+
+        while (!minHeap.empty()) {
+        
+            vector<int> curr = minHeap.top();
+            minHeap.pop();
+            int curMin = curr[0], listIdx = curr[1], elemIdx = curr[2];
+
+      
+            if ((curMax - curMin < smallRange[1] - smallRange[0]) || 
+                (curMax - curMin == smallRange[1] - smallRange[0] && curMin < smallRange[0])) {
+                smallRange[0] = curMin;
+                smallRange[1] = curMax;
+            }
+
+            if (elemIdx + 1 < nums[listIdx].size()) {
+                int nextVal = nums[listIdx][elemIdx + 1];
+                minHeap.push({nextVal, listIdx, elemIdx + 1});
+                curMax = max(curMax, nextVal); 
+            } else {
+         
+                break;
+            }
+        }
+        return smallRange;
+    }
+};
