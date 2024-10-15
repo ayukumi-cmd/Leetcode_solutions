89aec79a27732075c0a699bbd5ff805421be2ad6
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-10-15 11:16:04 +0530

    Time: 35 ms (43.16%), Space: 15.7 MB (45.68%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,14 +1,14 @@
-#define ll long long
 class Solution {
 public:
     long long minimumSteps(string s) {
-       ll n= s.length();
-       ll count=0;
-       ll ans=0;
-       for(ll i=n-1 ; i>=0; i--){
-       if(s[i]=='1'){ans+=count ; continue;}
-       else if(s[i]=='0'){count++;}
-       }
-       return ans;
+        long long swap = 0;
+        int black = 0; 
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '0') 
+                swap += (long long) black; 
+            else
+                black++; 
+        }
+        return swap;
     }
 };
