f0b6d7b87733da390f23b0c760485b827aa39927
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-10-19 07:21:13 +0530

    Time: 0 ms (100%), Space: 40.6 MB (84.35%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.java
+++ 1304-longest-happy-string/1304-longest-happy-string.java
@@ -1,56 +1,39 @@
 class Solution {
     public String longestDiverseString(int a, int b, int c) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> y[0] - x[0]); 
+        StringBuilder sb = new StringBuilder();
+        int currA = 0, currB = 0, currC = 0;  
 
-        if (a > 0) {
-            pq.offer(new int[]{a, 'a'});
-        }
-        if (b > 0) {
-            pq.offer(new int[]{b, 'b'});
-        }
-        if (c > 0) {
-            pq.offer(new int[]{c, 'c'});
-        }
-
-        StringBuilder result = new StringBuilder();
-
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int currCount = curr[0];
-            char currChar = (char) curr[1];
-
+        while (a > 0 || b > 0 || c > 0) {
             
-            if (result.length() >= 2 && result.charAt(result.length() - 1) == currChar 
-                    && result.charAt(result.length() - 2) == currChar) {
-                
-                if (pq.isEmpty()) {
-                    break; 
-                }
-
-  
-                int[] next = pq.poll();
-                int nextCount = next[0];
-                char nextChar = (char) next[1];
-                result.append(nextChar);
-                nextCount--;
-                
-                if (nextCount > 0) {
-                    pq.offer(new int[]{nextCount, nextChar});
-                }
-                
-            
-                pq.offer(curr);
-            } else {
-             
-                result.append(currChar);
-                currCount--;
-
-                if (currCount > 0) {
-                    pq.offer(new int[]{currCount, currChar});
-                }
+            if ((a >= b && a >= c && currA != 2) || (currB == 2 && a > 0) || (currC == 2 && a > 0)) {
+                sb.append('a');
+                a--;
+                currA++;
+                currB = 0;
+                currC = 0;
+            }
+          
+            else if ((b >= a && b >= c && currB != 2) || (currA == 2 && b > 0) || (currC == 2 && b > 0)) {
+                sb.append('b');
+                b--;
+                currB++;
+                currA = 0;
+                currC = 0;
+            }
+      
+            else if ((c >= a && c >= b && currC != 2) || (currA == 2 && c > 0) || (currB == 2 && c > 0)) {
+                sb.append('c');
+                c--;
+                currC++;
+                currA = 0;
+                currB = 0;
+            }
+            else {
+         
+                break;
             }
         }
 
-        return result.toString();
+        return sb.toString();
     }
 }
