b48c1fe82e2e7fc79aa446e16ce71be83b2eeaad
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-10-25 20:04:39 +0530

    Time: 0 ms (100%), Space: 19.3 MB (79.14%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,24 +1,22 @@
+
 class Solution {
 public:
-    vector<int> dp;
-    int solve(vector<int>& nums, int i, int steps) {
-        int n = nums.size();
-        if (i ==n - 1) return 0;
-        if(i>=n){return INT_MAX;}
+    int jump(vector<int>& nums) {
+        int n = nums.size(); 
+        if (n <= 1) return 0;
 
-        if (dp[i] != -1) return dp[i];
+        int max_jump = 0; 
+        int jumps = 0; 
+        int current_end = 0; 
 
-        int step = INT_MAX;
-        for (int j = 1; j <= nums[i]; j++) {
-            if (i + j < n and solve(nums, i + j, steps + 1)!=INT_MAX) {
-                step = min(step, 1 + solve(nums, i + j, steps + 1));
+        for (int i = 0; i < n - 1; i++) { 
+            max_jump = max(max_jump, i + nums[i]); 
+            if (i == current_end) { 
+                jumps++; 
+                current_end = max_jump; 
             }
         }
-        return dp[i] = step;
-    }
-    int jump(vector<int>& nums) {
-        int n = nums.size();
-        dp.resize(n+1, -1);
-        return solve(nums, 0, 0);
+
+        return jumps;
     }
 };
