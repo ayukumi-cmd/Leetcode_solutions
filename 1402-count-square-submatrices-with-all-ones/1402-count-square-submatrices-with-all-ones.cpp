c4ac381846df347afb6a817776e64db85f2844a6
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-10-28 16:51:42 +0530

    Time: 17 ms (9.93%), Space: 30.1 MB (5.34%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& matrix, int i, int j, int n, int m, vector<vector<int>>& dp) {
+    int solve(vector<vector<int>>& matrix, int i, int j, int n, int m, vector<vector<int>>& dp, int &x) {
         if (i >= n || j >= m) {
             return 0;
         }
         if (dp[i][j] != -1) return dp[i][j];
         
-        int right = solve(matrix, i, j + 1, n, m, dp);
-        int bottom = solve(matrix, i + 1, j, n, m, dp);
-        int diagonal = solve(matrix, i + 1, j + 1, n, m, dp);
+        int right = solve(matrix, i, j + 1, n, m, dp,x);
+        int bottom = solve(matrix, i + 1, j, n, m, dp,x);
+        int diagonal = solve(matrix, i + 1, j + 1, n, m, dp,x);
         
         if (matrix[i][j] == 1) {
             int ans = 1 + min({bottom, right, diagonal});
             dp[i][j] = ans;
+            x+=ans;
             return ans;
         } else {
             return dp[i][j] = 0;
@@ -25,12 +26,11 @@ public:
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
         int totalSquares = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                totalSquares += solve(matrix, i, j, n, m, dp);
-            }
-        }
+                int x=0;
+                totalSquares += solve(matrix, 0, 0, n, m, dp, x);
+           
+        
 
-        return totalSquares;
+        return x;
     }
 };
