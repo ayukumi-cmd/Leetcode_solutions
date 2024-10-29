9dca8586a9264f74cae6c2818d5d22a9b77f16e0
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-10-29 12:15:49 +0530

    Time: 15 ms (56.52%), Space: 74.6 MB (18.45%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
 
-    int solve(int i, int j, vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+    vector<vector<int>>dp;
 
-        if (j >= m) {
+    int solve(int i, int j, vector<vector<int>>&grid){
+        if(i>=grid.size() ||  j>= grid[0].size()){
             return 0;
         }
 
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
+        if(dp[i][j]!=-1){return dp[i][j];}
+        int el=grid[i][j]; 
+        int ans=0;
+        int n=grid.size(); 
+        int m=grid[0].size();
 
-        int el = grid[i][j];
-        int ans = 0;
+       if(i-1>=0 and j+1<m and el<grid[i-1][j+1]){ans= max(ans, 1+ solve(i-1, j+1,grid));}
+       if(i>=0 and j+1<m and el<grid[i][j+1]){ans= max(ans, 1+ solve(i, j+1,grid));}
+       if(i+1<n and j+1<m and el<grid[i+1][j+1]){ans= max(ans, 1+ solve(i+1, j+1,grid));}
 
-        // Check moves to the right diagonal, right, and right-down diagonal
-        if (i - 1 >= 0 && j + 1 < m && el < grid[i - 1][j + 1]) {
-            ans = max(ans, 1 + solve(i - 1, j + 1, grid));
-        }
-        if (j + 1 < m && el < grid[i][j + 1]) {
-            ans = max(ans, 1 + solve(i, j + 1, grid));
-        }
-        if (i + 1 < n && j + 1 < m && el < grid[i + 1][j + 1]) {
-            ans = max(ans, 1 + solve(i + 1, j + 1, grid));
-        }
 
-        return dp[i][j] = ans;
+        return dp[i][j]=ans;
     }
-
     int maxMoves(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int ans = 0; // Initialize answer to 0
-        dp.resize(n, vector<int>(m, -1));
-
-        for (int i = 0; i < n; i++) { // Loop over rows instead of columns
-            ans = max(ans, solve(i, 0, grid));
+        int n= grid.size(); 
+        int m=grid[0].size(); 
+        int ans= INT_MIN;
+        dp.resize(n+1, vector<int>(m+1, -1));
+        for(int i=0; i<n; i++){
+            ans= max(ans,solve(i, 0,grid));
         }
-
         return ans;
     }
 };
