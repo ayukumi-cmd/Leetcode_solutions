9d54b8672621d5cfcce50842765b240503731828
ayukumi-cmd <98808592+ayukumi-cmd@users.noreply.github.com>
2024-11-23 13:32:44 +0530

    Time: 7 ms (79.67%), Space: 79.5 MB (47.91%) - LeetHub

---
+++ 1972-rotating-the-box/1972-rotating-the-box.java
@@ -0,0 +1,28 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+        int r = box.length, c = box[0].length;
+        char[][] finalBox = new char[c][r];
+        
+        for(int i = 0; i<r; ++i){
+            int empty = c-1;
+            for(int j = c-1; j>=0; --j){
+                if(box[i][j] == '*'){
+                    empty = j-1;
+                     continue;
+                }
+                if(box[i][j] == '#'){
+                    box[i][j] = '.';
+                    box[i][empty] = '#';
+                    --empty;
+                }
+            }
+        }
+        
+        for(int i = 0; i<r; ++i){
+            for(int j = c-1; j>=0; --j)
+                finalBox[j][r-i-1] = box[i][j];
+        }
+        
+        return finalBox;
+    }
+}
